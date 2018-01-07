11ce849cc10972ce5b7c24f0f81521868726b9bb
Rover <rover@ustream.tv>
2018-01-07 23:49:25 +0100

    feat(importer): Parsing events implemented

--- importer/api.go
+++ importer/api.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
 // Feed describes the endpoints of the feed server
 type feed struct {
 	root    string
@@ -38,17 +44,35 @@ func NewAPI(feedHost, storeHost string, parser Parser) API {
 
 // GetRequest makes a simple get request for a given url and returns the body
 func (a API) GetRequest(url string) ([]byte, error) {
-	return nil, nil
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	var body []byte
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, err
 }
 
 // GetEvents is for making a request for event IDs
 func (a API) GetEvents() ([]int, error) {
-	return nil, nil
+	body, err := a.GetRequest(a.feed.events)
+	if err != nil {
+		return nil, err
+	}
+	return a.parser.Events(body)
 }
 
 // GetEventByID returns an Event for a given ID
 func (a API) GetEventByID(id int) (*Event, error) {
-	return nil, nil
+	url := fmt.Sprintf("%s/%d", a.feed.events, id)
+	body, err := a.GetRequest(url)
+	if err != nil {
+		return nil, err
+	}
+	return a.parser.Event(body)
 }
 
 // GetMarketByID returns a market for a given ID
--- importer/parser.go
+++ importer/parser.go
@@ -1,6 +1,11 @@
 package main
 
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"time"
+)
 
 // Parser describes a parser object which holds functions to parse values
 type Parser struct {
@@ -8,27 +13,93 @@ type Parser struct {
 
 // String parses a given value into string value
 func (p *Parser) String(value interface{}) string {
+	if v, ok := value.(string); ok {
+		return v
+	}
+	switch value.(type) {
+	case float64:
+		return strconv.Itoa(int(value.(float64)))
+	case int:
+		return fmt.Sprintf("%d", value.(int))
+	}
 	return ""
 }
 
 // Time parses a value into a given time form
 func (p *Parser) Time(value interface{}) (time.Time, error) {
-	return time.Now(), nil
+	if v, ok := value.(time.Time); ok {
+		return v, nil
+	}
+	switch value.(type) {
+	case float64:
+		return time.Unix(int64(value.(float64)), 0).UTC(), nil
+	case int:
+		return time.Unix(int64(value.(int)), 0).UTC(), nil
+	case string:
+		input := value.(string)
+		t, err := time.Parse(time.RFC3339, input)
+		if err == nil {
+			return t, nil
+		}
+		t, err = time.Parse("2006-01-02:15:00:00Z", input)
+		if err == nil {
+			return t, nil
+		}
+		t, err = time.Parse("2006-01-02 15:00:00Z", input)
+		if err == nil {
+			return t, nil
+		}
+		i, err := strconv.ParseInt(input, 10, 64)
+		if err == nil {
+			return time.Unix(int64(i), 0).UTC(), nil
+		}
+	}
+	return time.Now(), fmt.Errorf("No time parsing worked")
 }
 
 // IntArray is trying to parse integer arrays
 func (p *Parser) IntArray(value interface{}) ([]int, error) {
-	return nil, nil
+	data, err := json.Marshal(value)
+	if err != nil {
+		return nil, err
+	}
+	var parsed []int
+	if err = json.Unmarshal(data, &parsed); err != nil {
+		return nil, err
+	}
+	return parsed, err
 }
 
 // Events is to parse an array of events
 func (p *Parser) Events(body []byte) ([]int, error) {
-	return nil, nil
+	var events []int
+	err := json.Unmarshal(body, &events)
+	return events, err
 }
 
 // Event is to parse a single event object
 func (p *Parser) Event(body []byte) (*Event, error) {
-	return nil, nil
+	var raw map[string]interface{}
+	if err := json.Unmarshal(body, &raw); err != nil {
+		return nil, err
+	}
+	t, err := p.Time(raw["time"])
+	if err != nil {
+		return nil, err
+	}
+	markets, err := p.IntArray(raw["markets"])
+	if err != nil {
+		return nil, err
+	}
+	event := Event{
+		base: base{
+			ID:   p.String(raw["id"]),
+			Name: p.String(raw["name"]),
+		},
+		Time:    t,
+		Markets: markets,
+	}
+	return &event, nil
 }
 
 // Market parses a market from the given body
