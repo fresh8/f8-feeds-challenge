7548f44c87e85c3a97c459a6847e250657d122c8
Rover <rover@ustream.tv>
2018-01-08 00:00:57 +0100

    feat(importer): Events requested

--- importer/main.go
+++ importer/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"sync"
 
 	"github.com/Roverr/f8-feeds-challenge/importer/config"
 )
@@ -17,8 +18,37 @@ func logInfo(customMsg string) {
 
 func main() {
 	// Initialize configuration and API
-	_, err := config.NewConfig()
+	settings, err := config.NewConfig()
 	if err != nil {
 		log.Fatal(err)
 	}
+	api := NewAPI(settings.GetFeedURL(), settings.GetStoreURL(), Parser{})
+
+	// Get all events
+	events, err := api.GetEvents()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(events) == 0 {
+		fmt.Println("Events array was parsed as empty, exiting with 0")
+		return
+	}
+
+	// Start requesting events
+	var wg sync.WaitGroup
+	wg.Add(len(events))
+	var eventCollection []Event
+	for _, id := range events {
+		go func(eventID int) {
+			defer wg.Done()
+			logInfo(fmt.Sprintf("Requesting %d event\n", eventID))
+			event, err := api.GetEventByID(eventID)
+			if err != nil {
+				logError(fmt.Sprintf("Event error with id %d, might not existed", eventID), err)
+				return
+			}
+			eventCollection = append(eventCollection, *event)
+		}(id)
+	}
+	wg.Wait()
 }
